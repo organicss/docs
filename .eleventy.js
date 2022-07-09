98238db75c6e09857ed37d981e24f2ecfb98197f
Daniel Yuschick <yuschick@users.noreply.github.com>
2022-07-09 17:34:37 +0300

    feat: setup reset styles (#4)
    Co-authored-by: Daniel Yuschick <daniel.yuschick@gmail.com>

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
+const eleventySass = require('eleventy-sass');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const postcss = require('postcss');
+
 module.exports = function (config) {
     config.addNunjucksGlobal('getYear', function () {
         return new Date().getFullYear();
+    });
+
+    config.addPlugin(eleventySass, {
+        postcss: postcss([autoprefixer, cssnano]),
     });
 
     return {
