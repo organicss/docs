3aeba4bab99166710a785987bc4b1bc7c57901c7
Daniel Yuschick <yuschick@users.noreply.github.com>
2022-07-11 16:41:38 +0300

    feat: setup 11ty site (#9)
    Co-authored-by: Daniel Yuschick <daniel.yuschick@gmail.com>

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+const eleventySass = require('eleventy-sass');
+const postcss = require('postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
+module.exports = function (config) {
+    config.addPassthroughCopy('./src/assets');
+
+    config.addPlugin(syntaxHighlight);
+    config.addPlugin(eleventySass, {
+        postcss: postcss([autoprefixer, cssnano]),
+    });
+
+    return {
+        dir: {
+            input: 'src',
+            output: '_site',
+        },
+    };
+};
